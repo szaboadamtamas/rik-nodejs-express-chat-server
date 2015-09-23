c1741bcb3fac04ae0ca6f086a32c445477db3abe
Ádám Szabó <szabo.adam.tamas@gmail.com>
2015-09-23 14:50:01 +0200

    Room api register

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res) {
 
 app.use('/account', require('./account'));
 app.use('/users', require('./users'));
+app.use('/rooms', require('./rooms'));
 
 app.use(function(err, req, res, next) {
   if(err.code === 11000 || err.name === 'ValidationError') {
