b16ca54b82ce7263f1d979ed7be5982c08b7b1be
Ádám Szabó <szabo.adam.tamas@gmail.com>
2015-09-23 15:31:02 +0200

    Message broadcast

--- api/rooms/messages/index.js
+++ api/rooms/messages/index.js
@@ -3,6 +3,7 @@ var app = module.exports = express.Router({mergeParams: true});
 var Room = require('../model');
 var Message = require('./model');
 var Auth = require('../../auth');
+var server = require('../../../server');
 
 app.get('/', function (req, res, next) {
   Message.find({roomId: req.params.roomId})
@@ -21,6 +22,8 @@ app.post('/', Auth.isAuthenticated, function (req, res, next) {
     if (err) {
       return next(err);
     }
+    console.log("message["+message+"] emitted to room: ",message.roomId)
+    server.socketIO.in(message.roomId).emit('new message', message);
     res.json(message);
   });
 });
